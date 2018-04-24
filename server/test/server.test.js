89faaa99892177925fde52860a2cc63eddbf896c
undefinedmamun <bkmmamun@gmail.com>
2018-04-24 22:49:09 +0600

    test the DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -100,4 +100,45 @@ describe('GET/todos/:id', () => {
             .expect(404)
             .end(done);
     });
+});
+
+
+describe('DELETE/todos/:id', ()=>{
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err)
+                    return done(err);
+                
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBe(null);
+                    done();
+                }).catch((e)=>{
+                    done(e);
+                })
+            })
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)
+    })
+
+    it('should return 404 if Object id is invalid', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    })
 })
+
